1b70aea5c54d94d01c5f6eac25e9e9d9018cf74d
Ardhweb <sun.projectsdev@gmail.com>
2024-09-18 23:58:50 +0530

    Admin Panle Initi

---
+++ base/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class MyModel(BaseModel):
+    name = models.CharField(max_length=100)
+    # other fields
